cceb3975212118e5e4dceb0da744a2a809f6ec1b
iamjethro07 <jethropaul2007@gmail.com>
2026-03-19 16:22:58 +0530

    initial commit

--- backend/app.py
+++ backend/app.py
@@ -29,6 +29,10 @@ def create_app():
 
     return app
 
+
+app = create_app()
+
+
+# (optional - only for local run)
 if __name__ == '__main__':
-    app = create_app()
     app.run(host='0.0.0.0', port=5000, debug=True)
